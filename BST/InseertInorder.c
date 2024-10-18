ae4d2be3d6e1e08725bf2196c8e0b86726b32a04
Ratemo Lewis <ratemolewis@gmail.com>
2024-10-18 14:27:44 +0300

    Implement recursive search and delete function

--- BST/InseertInorder.c
+++ BST/InseertInorder.c
@@ -43,7 +43,27 @@ void Insert(int key)
         
 }
 
-void Inorder(struct Node *p)//recursive function
+struct Node * RInsert(struct Node *p, int key)
+{
+    struct Node *t;
+    
+    if(p==NULL)
+    {
+        t=(struct Node*)malloc(sizeof(struct Node));
+        t->data=key;
+        t->lchild=t->rchild=NULL;
+        return t;
+    }
+     if(key < p->data)
+        p->lchild=RInsert(p->lchild, key);
+     else if(key > p->data)
+        p->rchild=RInsert(p->rchild,key);
+        
+        return p;
+
+}
+
+void Inorder(struct Node *p)//recursive ftunction
 {
     if(p)//if p becomes 0, then terminate
     {
@@ -70,15 +90,79 @@ struct Node * Search(int key)
    
 }
 
+int Height(struct Node *p)
+{  int x, y;
+    if(p==NULL)//stoping condition.
+        return 0;//when we reach the leaf node, return 
+
+   x=Height(p->lchild);
+   y=Height(p->rchild);
+  // if(x>y) return x+1;else return y+1;
+  return x>y?x+1:y+1;
+
+}
+
+struct Node *InPre(struct Node *p)
+{
+     while(p && p->rchild!=NULL)
+        p=p->rchild;
+
+    return p;
+}
+
+struct Node *InSucc(struct Node *p)
+{
+     while(p && p->lchild!=NULL)
+        p=p->lchild;
+
+    return p;
+}
+
+struct Node *Delete(struct Node *p,int key)
+{
+    struct Node *q;
+    if(p==NULL)//if p has become NULL, stop the process/delete
+        return NULL;
+    if(p->lchild==NULL && p->rchild==NULL)//delete this node,
+    {
+        if(p==root)
+            root=NULL;
+        free(p);// this is were delete is done, when we reach the leaf node.
+        return NULL;
+    }
+
+    if(key<p->data)
+        p->lchild=Delete(p->rchild,key);
+    else if(key>p->data)
+        p->rchild=Delete(p->rchild,key);
+    else
+    {
+        if(Height(p->lchild)>Height(p->rchild))
+        {
+            q=InPre(p->lchild);
+            p->data=q->data;
+            p->lchild=Delete(p->lchild,q->data);
+        }
+        else
+        {
+            q=InSucc(p->rchild);
+            p->data=q->data;
+            p->rchild=Delete(p->rchild,q->data);
+        }
+    }
+    return p;
+}
+
 int main()
 {
     struct Node *temp;
-    Insert(10);
-    Insert(5);
-    Insert(20);
-    Insert(8);
-    Insert(30);
+    root=RInsert(root,50);
+    RInsert(root,10);
+    RInsert(root,40);
+    RInsert(root,20);
+    RInsert(root,30);
 
+    Delete(root,50);
     Inorder(root);
     printf("\n");
 
